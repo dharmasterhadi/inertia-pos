f707931af66948c3f0b8094a5fad9eb5ec8fa1cb
dharmasterhadi <ady.dha851@gmail.com>
2023-11-19 20:39:42 +0700

    until membuat proses logout

---
+++ resources/js/Layouts/Auth.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="c-app flex-row align-items-center">
+        <div class="container">
+            <div class="row justify-content-center">
+                <slot />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,32 @@
 import './bootstrap';
+
+
+import { createApp, h } from 'vue';
+import { createInertiaApp } from '@inertiajs/vue3';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+ 
+createInertiaApp({
+  resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+  setup({ el, App, props, plugin }) {
+    return createApp({ render: () => h(App, props) })
+      .mixin({
+        methods: {
+          //method "hasAnyPermission"
+          hasAnyPermission : function (permissions){
+
+            //get permissions from props
+            let allPermissions = this.$page.props.auth.permissions;
+
+            let hasPermission = false;
+            permissions.forEach(function(item){
+              if(allPermissions[item]) hasPermission = true;
+            });
+
+            return hasPermission;
+          }
+        }
+      })
+      .use(plugin)
+      .mount(el)
+  },
+});
